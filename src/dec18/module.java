75b63ba2398737471bc1437b31822c71862cc2fc
Shiffali914 <Shiffalibansal914@gmail.com>
2024-12-19 15:26:43 -0500

    new correction

--- src/dec18/module.java
+++ src/dec18/module.java
@@ -3,5 +3,6 @@ package dec18;
 public class module {
         public static void main(String[] args) {
             System.out.println("Hello World");
+            System.out.println("hi");
         }
 }
